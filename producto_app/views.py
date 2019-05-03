1eda2212e1e96ce21e12278022ea119ab7c60026
jhonedin <jhonedin1888@hotmail.com>
2019-05-03 13:27:09 -0500

    segundo commit

---
+++ producto_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+#def getProducto_asin(request):
+
+	#return render(request, 'partido/partido.html',{'partidos':partidos})
+
---
+++ tienda_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Home(request):
+	return render(request, 'home.html',{})
+
+
+def Tienda(request):
+	return render(request, 'tienda.html',{})
