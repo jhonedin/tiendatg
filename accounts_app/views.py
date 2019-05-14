c620ee44c5423479012005b2b78d7edb2a77c0df
jhonedin <jhonedin1888@hotmail.com>
2019-05-13 19:07:09 -0500

    Se logro crear el login y mostrar el name y id de usuario en la vista home

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from accounts_app.forms import LoginForm
 # Create your views here.
 
 from producto_app.models import ReviewsAmazonDataset # importo el modelo del review del dataset que esta en la BD
 
 def login(request):
-	#user = get_object_or_404(ReviewsAmazonDataset, reviewerid = request.POST['password'], reviewername = request.POST['username'] )
-	return render(request, 'accounts_app/login.html',{})
-
-
+	if request.method == 'POST':
+		user = get_object_or_404(ReviewsAmazonDataset, reviewerid = request.POST['reviewerid'], reviewername = request.POST['reviewername'] )
+		if user is not None:
+			return render(request, 'tienda_app/home.html',{'user':user})
+	else:
+		form = LoginForm(request.POST)
+	return render(request, 'accounts_app/login.html',{'form':form})
