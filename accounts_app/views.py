a3945249aa9e89909c6fafede890d421d6a0ab84
jhonedin <jhonedin1888@hotmail.com>
2019-07-02 21:43:14 -0500

    se trabajo en el registro de usuarios nuevo con consulta sql

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -4,6 +4,7 @@ from accounts_app.forms import RegistroForm
 # Create your views here.
 
 from producto_app.models import ReviewsAmazonDataset # importo el modelo del review del dataset que esta en la BD
+import psycopg2 # importo la libreria para la conexion a la base de datos
 
 userConsultaGlobal = []
 
@@ -27,10 +28,25 @@ def login(request):
 
 def registro(request):
 	if request.method == 'POST':
-		nombreNuevoReg = request.POST['reviewername']
-		idNuevoReg = request.POST['reviewerid']
-		print("nombre nuevo reg: "+nombreNuevoReg)
-		print("id nuevo reg: "+idNuevoReg)
+		reviewerName = request.POST['reviewername']
+		reviewerID = request.POST['reviewerid']
+		asin = '-1'
+		overall = '-1'
+		datos = []
+		datos.append(reviewerID)
+		datos.append(asin)
+		datos.append(reviewerName)
+		datos.append(overall)
+		try:
+			conn = psycopg2.connect("dbname='tienda_bd' user='postgres' host='localhost' password='jhon'")
+			print("Conexion a la base de datos exitosa desde registro \n")
+		except:
+			print ("I am unable to connect to the database desde registro")
+		cursor = conn.cursor()
+		cursor.execute("INSERT into reviews_amazon_dataset(reviewerID,asin,reviewerName,overall) VALUES (%s, %s,%s,%s)",datos)
+		conn.commit()
+		cursor.close()
+		print("Registro exitoso desde registro")
 		form = LoginForm(request.POST)
 		return render(request, 'accounts_app/login.html',{'form':form})
 	else:
